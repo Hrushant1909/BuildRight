a3c393273d93d8a0b28779b9c7f1972aec9785c8
Hrushant Motinge <155289678+Hrushant1909@users.noreply.github.com>
2026-04-21 19:53:16 +0530

    updated start.sh-3

--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-echo "Starting app..."
+echo "===== STARTING APPLICATION ====="
 
-export DB_URL=jdbc:mysql://database-2.cfugowg0e22z.ap-south-1.rds.amazonaws.com:3306/buildright
+export DB_URL=jdbc:mysql://database-2.cfugowg0e22z.ap-south-1.rds.amazonaws.com:3306/<YOUR_DB_NAME>
 export DB_USER=admin
 export DB_PASS=hrushant1909
 
-cd /home/ec2-user
+cd /home/ec2-user || exit
 
+echo "Files:"
+ls -l
+
+echo "Stopping old app..."
 pkill -f jar || true
 
-nohup java -jar app.jar > app.log 2>&1 &
+echo "Running app in foreground (DEBUG)..."
 
-echo "Done"
+java -jar app.jar
+
+echo "===== END ====="
