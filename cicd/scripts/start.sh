a2f5d84e746f32321eb5cbdf8705c35faa8a0de8
Hrushant Motinge <155289678+Hrushant1909@users.noreply.github.com>
2026-04-21 23:21:22 +0530

    backend-buildspec fix-6

--- cicd/scripts/start.sh
+++ cicd/scripts/start.sh
@@ -2,7 +2,7 @@
 
 echo "===== STARTING APPLICATION ====="
 
-export DB_URL=jdbc:mysql://database-2.cfugowg0e22z.ap-south-1.rds.amazonaws.com:3306/<YOUR_DB_NAME>
+export DB_URL=jdbc:mysql://database-2.cfugowg0e22z.ap-south-1.rds.amazonaws.com:3306/buildright
 export DB_USER=admin
 export DB_PASS=hrushant1909
 
